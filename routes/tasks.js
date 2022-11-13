ba504273d87b36c528fa1819d45e2d5fce87c668
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-13 10:55:30 +0100

    new: task route setup

---
+++ routes/tasks.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const taskRouter = express.Router()
+
+const {
+    createTask,
+    getAllTasks,
+    getTask,
+    updateTask,
+    deleteTask
+} = require("../controllers/tasks")
+
+taskRouter
+    .route("/")
+    .get(getAllTasks)
+    .post(createTask)
+
+taskRouter
+    .route("/:id")
+    .get(getTask)
+    .patch(updateTask)
+    .delete(deleteTask)
+
+ 
