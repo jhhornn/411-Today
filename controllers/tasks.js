916114455132e5aecf5a1f75db5382435b5314f1
Awosise Oluwaseun <awosiseo@gmail.com>
2022-11-13 10:54:57 +0100

    new: task controller skeleton

---
+++ controllers/tasks.js
@@ -0,0 +1,28 @@
+const createTask = (req, res) => {
+    
+}
+
+const getAllTasks = (req, res) => {
+
+}
+
+const getTask = (req, res) => {
+
+}
+
+const updateTask = (req, res) => {
+
+}
+
+const deleteTask = (req, res) => {
+
+}
+
+module.exports = {
+    createTask,
+    getAllTasks,
+    getTask,
+    updateTask,
+    deleteTask
+}
+
